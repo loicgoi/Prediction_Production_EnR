aafd19bd2047f566e74950a86028c0b88b28efbb
Chaima425 <chaimaabdraouf@gmail.com>
2025-10-01 11:15:59 +0200

    data_acquisition_solaire_fini

--- src/data_ingestion/fetch_open_meteo.py
+++ src/data_ingestion/fetch_open_meteo.py
@@ -1,14 +1,15 @@
-# soleil ######
+#######-solaire######
 import requests
 import pandas as pd
 
 # URL MP Open-Meteo
 url = "https://api.open-meteo.com/v1/forecast"
 params = {
-    "latitude": 43.6119,
-    "longitude": 3.8777,
-    "daily": "temperature_2m_max,temperature_2m_min,windspeed_10m_max,shortwave_radiation_sum,precipitation_sum",
-    "timezone": "Europe/Paris"
+	"latitude": 43.6109,
+	"longitude": 3.8763,
+	"daily": ["cloud_cover_max", "cloud_cover_min", "temperature_2m_mean", "apparent_temperature_mean", "shortwave_radiation_sum", "sunshine_duration", "cloud_cover_mean", "uv_index_max", "uv_index_clear_sky_max", "precipitation_sum", "precipitation_hours", "relative_humidity_2m_mean", "wind_speed_10m_mean", "wind_gusts_10m_mean", "sunrise", "sunset", "daylight_duration", "temperature_2m_max", "temperature_2m_min"],
+	"current": "cloud_cover",
+	"forecast_days": 16,
 }
 
 # Récupération des données
@@ -18,3 +19,4 @@ data = response.json()
 # Transformation en DataFrame
 df = pd.DataFrame(data["daily"])
 print(df.head())
+#############------#############
