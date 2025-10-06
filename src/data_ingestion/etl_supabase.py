ef181408d4c88e4a69830849bb5a807c82d27f12
[loicgoi] <g.loic.cours@gmail.com>
2025-10-06 15:56:23 +0200

    refacto code + handler production

--- src/data_ingestion/etl_supabase.py
+++ src/data_ingestion/etl_supabase.py
@@ -1,7 +1,13 @@
 from abc import ABC, abstractmethod
 import pandas as pd
 from supabase import create_client
+from ..config.settings import settings
+import logging
 import os
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 class DataHandler(ABC):
@@ -9,8 +15,8 @@ class DataHandler(ABC):
     Classe abstraite pour gérer le chargement, le nettoyage et la sauvegarde des données.
     """
 
-    def __init__(self, supabase_url: str, supabase_key: str):
-        self.client = create_client(supabase_url, supabase_key)
+    def __init__(self):
+        self.client = create_client(settings.supabase_url, settings.supabase_key)
         self.pd = pd.DataFrame()
 
     @abstractmethod
@@ -26,51 +32,79 @@ class DataHandler(ABC):
         """
         pass
 
-    def save_to_db(self, table_name: str):
+    def _infert_sql_type(self, pd_type):
+        """
+        Infère le type SQL correspondant à un type de données pandas.
+
+        Cette méthode prend un type pandas (`pd_type`) et renvoie
+        le type SQL le plus approprié pour ce type.
+
+        Types pandas gérés :
+            - Integer   -> "INTEGER"
+            - Float     -> "FLOAT"
+            - Boolean   -> "BOOLEAN"
+            - Datetime  -> "TIMESTAMP"
+            - Autres    -> "TEXT"
+
+        Paramètres
+        ----------
+        pd_type : pandas.api.types.CategoricalDtype, numpy.dtype ou similaire
+            Le type de la colonne pandas à convertir en type SQL.
+
+        Retour
+        ------
+        str
+            Le type SQL correspondant sous forme de chaîne de caractères.
+        """
+        if pd.api.types.is_integer_dtype(pd_type):
+            return "INTEGER"
+        elif pd.api.types.is_float_dtype(pd_type):
+            return "FLOAT"
+        elif pd.api.types.is_bool_dtype(pd_type):
+            return "BOOLEAN"
+        elif pd.api.types.is_datetime64_any_dtype(pd_type):
+            return "TIMESTAMP"
+        else:
+            return "TEXT"
+
+    def save_to_db(self, table_name: str, if_exists="append"):
         """
         Sauvegarde le DataFrame dans Supabase avec création automatique de table/colonne.
         """
         if self.df.empty:
-            self.df = self.load()
-            self.clean()
+            logging.warning(f"Dataframe vide pour la table '{table_name}'.")
+            return
 
         df_to_insert = self.df.copy()
-        df_to_insert = df_to_insert.astype(str)
-        columns = df_to_insert.columns.tolist()
+        for col in df_to_insert.columns:
+            if pd.api.types.is_datetime64_any_dtype(df_to_insert[col]):
+                df_to_insert[col] = df_to_insert[col].dt.strftime("%Y-%m-%d")
 
-        tables = (
-            self.client.table("information_schema.table")
-            .select("table_name")
-            .execute()
-            .data
-        )
-        table_names = [table["table_name"] for table in tables]
+        try:
+            self.client.table(table_name).select("*").limit(1).execute()
+            table_exists = True
+        except Exception:
+            table_exists = False
 
-        if table_name not in table_names:
-            cols_def = ", ".join([f"{col} TEXT" for col in columns])
-            self.client.rpc(
-                "sql", {"query": f"CREATE TABLE {table_name} ({cols_def});"}
-            ).execute()
-        else:
-            existing_cols_info = (
-                self.client.table("information_schema.columns")
-                .select("column_name")
-                .eq("table_name", table_name)
-                .execute()
-                .data
-            )
-            existing_cols = [column["column_name"] for column in existing_cols_info]
+        if table_exists and if_exists == "append":
+            pass
+        elif not table_exists or if_exists == "replace":
+            if table_exists and if_exists == "replace":
+                self.client.rpc(
+                    "sql", {"query": f"DROP TABLE IF EXISTS {table_name};"}
+                ).execute()
 
-            for col in columns:
-                if col not in existing_cols:
-                    self.client.rpc(
-                        "sql",
-                        {"query": f"ALTER TABLE {table_name} ADD COLUMN {col} TEXT;"},
-                    ).execute()
+            cols_def = [
+                f"{col} {self._infert_sql_type(self.df[col].dtype)}"
+                for col in df_to_insert.columns
+            ]
+            create_table_query = f"CREATE TABLE {table_name} ({', '.join(cols_def)});"
+            self.client.rpc("sql", {"query": create_table_query}).execute()
+            logging.info(f"Table '{table_name}' créée.")
 
         records = df_to_insert.to_dict(orient="records")
         self.client.table(table_name).insert(records).execute()
-        print(f"Données insérées dans la table '{table_name}'")
+        logging.info(f"{len(records)} enregistrements insérés dans '{table_name}'.")
 
 
 class CSVDataHandler(DataHandler):
@@ -78,8 +112,8 @@ class CSVDataHandler(DataHandler):
     Gestionnaire pour charger des CSV locaux.
     """
 
-    def __init__(self, file_path: str, supabase_url: str, supabase_key: str):
-        super().__init__(supabase_url, supabase_key)
+    def __init__(self, file_path: str):
+        super().__init__()
         self.file_path = file_path
 
     def load(self) -> pd.DataFrame:
@@ -94,8 +128,8 @@ class APIDataHandler(DataHandler):
     Gestionnaire pour charger des données via API.
     """
 
-    def __init__(self, loader_function, supabase_url: str, supabase_key: str, **kwargs):
-        super().__init__(supabase_url, supabase_key)
+    def __init__(self, loader_function, **kwargs):
+        super().__init__()
         self.loader_function = loader_function
         self.loader_kwargs = kwargs
 
