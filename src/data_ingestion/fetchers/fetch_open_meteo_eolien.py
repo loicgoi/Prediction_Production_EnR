8c2ffa5bfc90fe37bf9e4f5113432b086d769389
Chaima425 <chaimaabdraouf@gmail.com>
2025-10-15 10:01:25 +0200

    fiixe

--- src/data_ingestion/fetchers/fetch_open_meteo_eolien.py
+++ src/data_ingestion/fetchers/fetch_open_meteo_eolien.py
@@ -3,15 +3,7 @@ import requests
 
 
 def get_wind_forecast(
-<<<<<<< HEAD
-<<<<<<< HEAD
     latitude: float, longitude: float, start_date: str = None, end_date: str = None
-=======
-    latitude: float, longitude: float, start_date: str, end_date: str
->>>>>>> 6242f1e (restructuration des fichiers + tests fonctionnels)
-=======
-    latitude: float, longitude: float, start_date: str = None, end_date: str = None
->>>>>>> 65ccff1 (refacto code + ajout du main.py fonctionnel)
 ) -> pd.DataFrame:
     """
     Prévisions éoliennes journalières via Open-Meteo API.
@@ -20,13 +12,7 @@ def get_wind_forecast(
     params = {
         "latitude": latitude,
         "longitude": longitude,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         "daily": "wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant,wind_gusts_10m_mean,temperature_2m_mean,surface_pressure_mean,cloud_cover_mean",
-=======
-        "daily": "wind_speed_10m_max,wind_speed_10m_mean,wind_gusts_10m_max,wind_direction_10m_dominant,wind_gusts_10m_mean,temperature_2m_mean,surface_pressure_mean,cloud_cover_mean",
->>>>>>> a45646a (correction problème de double import dans supabase + correction dans la séparation raw / clean)
         "timezone": "Europe/Paris",
         "forecast_days": 16,
     }
@@ -40,39 +26,6 @@ def get_wind_forecast(
         df["time"] = pd.to_datetime(df["time"])
 
     return df
-=======
-        "start_date": start_date,
-        "end_date": end_date,
-        "daily": [
-            "wind_speed_10m_max",
-            "wind_gusts_10m_max",
-            "wind_direction_10m_dominant",
-            "wind_gusts_10m_mean",
-            "temperature_2m_mean",
-            "surface_pressure_mean",
-            "cloud_cover_mean",
-        ],
-=======
-        "daily": "wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant",
->>>>>>> 65ccff1 (refacto code + ajout du main.py fonctionnel)
-        "timezone": "Europe/Paris",
-        "forecast_days": 16,
-    }
-
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-<<<<<<< HEAD
-    return pd.DataFrame(response.json()["daily"])
->>>>>>> 6242f1e (restructuration des fichiers + tests fonctionnels)
-=======
-    data = response.json()
-
-    df = pd.DataFrame(data["daily"])
-    if "time" in df.columns:
-        df["time"] = pd.to_datetime(df["time"])
-
-    return df
->>>>>>> 65ccff1 (refacto code + ajout du main.py fonctionnel)
 
 
 def get_wind_history(
@@ -87,13 +40,7 @@ def get_wind_history(
         "longitude": longitude,
         "start_date": start_date,
         "end_date": end_date,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         "daily": "wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant,wind_gusts_10m_mean,temperature_2m_mean,surface_pressure_mean,cloud_cover_mean",
-=======
-        "daily": "wind_speed_10m_max,wind_speed_10m_mean,wind_gusts_10m_max,wind_direction_10m_dominant,wind_gusts_10m_mean,temperature_2m_mean,surface_pressure_mean,cloud_cover_mean",
->>>>>>> a45646a (correction problème de double import dans supabase + correction dans la séparation raw / clean)
         "timezone": "Europe/Paris",
     }
 
@@ -106,32 +53,3 @@ def get_wind_history(
         df["time"] = pd.to_datetime(df["time"])
 
     return df
-=======
-        "daily": [
-            "wind_speed_10m_max",
-            "wind_gusts_10m_max",
-            "wind_direction_10m_dominant",
-            "cloud_cover_mean",
-            "surface_pressure_mean",
-            "temperature_2m_mean",
-        ],
-=======
-        "daily": "wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant",
->>>>>>> 65ccff1 (refacto code + ajout du main.py fonctionnel)
-        "timezone": "Europe/Paris",
-    }
-
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-<<<<<<< HEAD
-    return pd.DataFrame(response.json()["daily"])
->>>>>>> 6242f1e (restructuration des fichiers + tests fonctionnels)
-=======
-    data = response.json()
-
-    df = pd.DataFrame(data["daily"])
-    if "time" in df.columns:
-        df["time"] = pd.to_datetime(df["time"])
-
-    return df
->>>>>>> 65ccff1 (refacto code + ajout du main.py fonctionnel)
